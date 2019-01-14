fa42976423099f86e07d1f56c69a7471e197bbfc
github username <github email address>
2019-01-14 01:39:03 -0500

    more front end styling, modal done, connects with notebook info

--- src/components/noteForm.js
+++ src/components/noteForm.js
@@ -9,13 +9,13 @@ export default class NoteForm extends Component {
   }
 
   handleSubmit = evt => {
-    evt.preventDefault();
-    debugger
     const { addNotes } = this.props;
     const newNote = {...this.state}
     addNotes(newNote);
-    
-    console.log(evt);
+    this.setState({ title: "" }, () => {
+      this.setState({ content: "" })
+    })
+    evt.preventDefault();
   }
 
   handleChange = evt => {
@@ -23,11 +23,12 @@ export default class NoteForm extends Component {
   }
 
   render() {
+    const { title, content } = this.state;
     return (
       <div className="formbox">
         <form className="formbody" onSubmit={this.handleSubmit}>
-          <input id="note-title" className="form-spacer" type="text" placeholder="type a title" name="title" onChange={this.handleChange}/>
-          <textarea id="note-content" className="form-spacer" name="content" placeholder="note content goes here..." cols="" rows="100"onChange={this.handleChange}/>
+          <input value={title} id="note-title" className="form-spacer" type="text" placeholder="type a title" name="title" onChange={this.handleChange}/>
+          <textarea value={content} id="note-content" className="form-spacer" name="content" placeholder="note content goes here..." cols="" rows="100"onChange={this.handleChange}/>
           <button className="button-primary" type="submit" value="submit">Submit</button>
         </form>
       </div>)
